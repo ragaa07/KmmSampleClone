e1cf8066e6b5fa1282337c7e0f9864e5f1f6330b
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2021-07-27 14:54:27 +0200

    chore: Extract the compose version to variable in build.gradle file

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,26 +6,28 @@ plugins {
 }
 group = "com.jetbrains"
 version = "1.0-SNAPSHOT"
-
 repositories {
     gradlePluginPortal()
     google()
     mavenCentral()
 }
+
+val composeVersion = "1.0.0-rc02"
+
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.compose.ui:ui:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("com.github.zerobranch:SwipeLayout:1.3.1")
     implementation("de.charlex.compose:revealswipe:1.0.0-beta05")
-    implementation("androidx.compose.material:material:1.0.0-rc02")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-rc02")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-rc02")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
 android {
     compileSdkVersion(30)
@@ -48,7 +50,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-rc02"
+        kotlinCompilerExtensionVersion = composeVersion
         kotlinCompilerVersion = "1.4.32"
     }
     compileOptions {
