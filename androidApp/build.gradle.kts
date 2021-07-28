86a42a374c6a9ffb38b6aa69c1c41d48a160fd71
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2021-07-28 11:01:42 +0200

    Add compose foundation and compose icons as dependencies in build.gradle

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,14 +19,22 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("com.github.zerobranch:SwipeLayout:1.3.1")
     implementation("de.charlex.compose:revealswipe:1.0.0-beta05")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    // Material Design
+    implementation("androidx.compose.material:material:$composeVersion")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    // Material design icons
+    implementation("androidx.compose.material:material-icons-core:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    // Integration with activities
     implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
 android {
