f95a331f31dd09331e667288fd14d80365372022
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2021-08-12 13:56:28 +0200

    chore: Update Compose to version 1.0.1 and Kotlin to version 1.5.21
    * Updated jetpack Compose to version 1.0.1 and activity-compose to 1.3.1
    
    * Updated kotlin to 1.5.21 from 1.5.10

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
     mavenCentral()
 }
 
-val composeVersion = "1.0.0"
+val composeVersion = "1.0.1"
 
 dependencies {
     implementation(project(":shared"))
@@ -33,7 +33,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     // Integration with activities
-    implementation("androidx.activity:activity-compose:1.3.0")
+    implementation("androidx.activity:activity-compose:1.3.1")
     // UI Tests
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
